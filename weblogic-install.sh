fd4f8fbe62c4bf0aac8785dc52ebbfafe659f94e
uday <udaykrd33@gmail.com>
2022-07-28 18:48:52 -0500

    Update weblogic-install.sh

--- weblogic-install.sh
+++ weblogic-install.sh
@@ -2,6 +2,8 @@
 sudo groupadd -g 54321 oci_fmw
 sudo useradd -u 54321 -g oci_fmw oci_fmw
 sudo su - oci_fmw
+sudo git yuminstall -y
+git clone weblogic12c-setup-files
 sudo tar -xvf jdk-8u241-linux-x64.tar.gz 
 sudo mkdir -p /usr/java
 sudo mv jdk1.8.0_241 /usr/java/
@@ -19,4 +21,26 @@ echo "export MW_HOME=/u01/app/oracle/middleware" > /home/oci_fmw/.bash_profile
 echo "export WLS_HOME=$MW_HOME/wlserver" >> /home/oci_fmw/.bash_profile
 echo "export WL_HOME=$WLS_HOME" >> /home/oci_fmw/.bash_profile
 echo "export PATH=$JAVA_HOME/bin:$PATH" >> /home/oci_fmw/.bash_profile
+cd /home/oci_fmw/software/
+git
 touch /home/oci_fmw/software/wls.rsp
+
+[ENGINE]
+Response File Version=1.0.0.0.0
+[GENERIC]
+ORACLE_HOME=/home/oci_fmw/oracle/middleware
+INSTALL_TYPE=WebLogic Server
+MYORACLESUPPORT_USERNAME=
+MYORACLESUPPORT_PASSWORD=<SECURE VALUE>
+DECLINE_SECURITY_UPDATES=true
+SECURITY_UPDATES_VIA_MYORACLESUPPORT=false
+PROXY_HOST=
+PROXY_PORT=
+PROXY_USER=
+PROXY_PWD=<SECURE VALUE>
+COLLECTOR_SUPPORTHUB_URL=
+
+vi /u01/software/oraInst.loc
+
+inventory_loc=/home/opc/oraInventory
+inst_group=oinstall
